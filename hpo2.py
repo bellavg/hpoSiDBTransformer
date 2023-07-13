858764287027bb373498ccb5bc290b7633e9504c
bellavg <isabella.v.gardner@gmail.com>
2023-07-13 18:00:15 +0200

    plots

--- hpo2.py
+++ hpo2.py
@@ -40,8 +40,8 @@ def objective(trial: optuna.trial.Trial):
     logger = CSVLogger(version="trial"+str(trial.number), save_dir="/home/igardner/hpologsnew2", name="hpotrials")
     model_config = define_model_config(trial)
     model = LitModel(model_config)
-    trainer = pl.Trainer(max_epochs=10,strategy='ddp_find_unused_parameters_true',  logger=logger,  enable_checkpointing=False, enable_progress_bar=False,
-                         check_val_every_n_epoch=10, limit_val_batches=10, limit_train_batches=50)
+    trainer = pl.Trainer(max_epochs=20,strategy='ddp_find_unused_parameters_true',  logger=logger,  enable_checkpointing=False, enable_progress_bar=False,
+                         check_val_every_n_epoch=20, limit_val_batches=10, limit_train_batches=50)
     trainer.logger.log_hyperparams(model_config)
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
     return trainer.callback_metrics["val_acc"].item()
@@ -49,7 +49,7 @@ def objective(trial: optuna.trial.Trial):
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize", study_name="SiDBTransformer_Hyperparameters", sampler=optuna.samplers.RandomSampler())
-    study.optimize(objective, n_trials=200, timeout=3600, gc_after_trial=True)
+    study.optimize(objective, n_trials=50, timeout=14400, gc_after_trial=True)
     trials_df = study.trials_dataframe()
     trials_df.to_csv("/home/igardner/hpologsnew2/hpotrials.csv")
     print(study.best_trial)
@@ -58,4 +58,4 @@ if __name__ == "__main__":
     fig = optuna.visualization.plot_parallel_coordinate(study, target_name = "val_acc")
     # Save the figure
     # pip install -U kaleido
-    fig.write_image("/home/igardner/hpologsnew2/optimization_plot.png")
+    fig.write_image("/home/igardner/hpologsnew2/randomoptimization_plot.png")
